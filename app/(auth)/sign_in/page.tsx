41fbcd694da0453a3066f36858e0b9cd4a07e97a
shreeyansh64 <shreeyansh64@gmail.com>
2026-01-29 13:29:00 +0530

    feat(auth): add authentication routes

---
+++ app/(auth)/sign_in/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Signin</div>
+  )
+}
+
+export default page
---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Page = ()=>{
+  return (
+    <div className="text-3xl">Pannl</div>
+  )
+}
+
+export default Page
